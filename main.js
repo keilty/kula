79d00582eb0da5e1720d701dfdb2d969b4a62933
Gabriel Keilty <gabrielpkeilty@gmail.com>
2021-05-28 10:24:34 -0300

    feat: add carousel arrows

--- main.js
+++ main.js
@@ -13,23 +13,30 @@ carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 //Buttons Listeners
 nextBtn.addEventListener('click', () => {
+  if (counter >= carouselImages.length - 1) return;
   carouselSlide.style.transition = 'transform 0.4s ease-in-out';
   counter++;
   carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
 
 prevBtn.addEventListener('click', () => {
+  if (counter <= 0) return;
   carouselSlide.style.transition = 'transform 0.4s ease-in-out';
   counter--;
   carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
 
  carouselSlide.addEventListener('transitionend', () => {
-   console.log(carouselImages[counter]);
    if (carouselImages[counter].id === 'last-clone') {
     carouselSlide.style.transition = "none";
     counter = carouselImages.length - 2;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
    }
+    if (carouselImages[counter].id === 'first-clone') {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+   }
+
  });
 
