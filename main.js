50c8dab23728eb944d43c1b96845363fd6521d1a
Gabriel Keilty <gabrielpkeilty@gmail.com>
2021-05-28 09:43:42 -0300

    feat: create carousel in obras section

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
-let variable = loguear;
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
 
-console.log(variable);
+//Buttons
+const prevBtn = document.querySelector('#prev-btn');
+const nextBtn = document.querySelector('#next-btn');
+
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//Buttons Listeners
+nextBtn.addEventListener('click', () => {
+  carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+  counter++;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', () => {
+  carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+  counter--;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+ carouselSlide.addEventListener('transitionend', () => {
+   console.log(carouselImages[counter]);
+   if (carouselImages[counter].id === 'last-clone') {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - 2;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+   }
+ });
+
