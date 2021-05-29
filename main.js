67aebb8797ea4c5121f6a86f68e969b304b1601b
Gabriel Keilty <gabrielpkeilty@gmail.com>
2021-05-29 10:51:20 -0300

    test: leaflet api

--- main.js
+++ main.js
@@ -9,34 +9,48 @@ const nextBtn = document.querySelector('#next-btn');
 let counter = 1;
 const size = carouselImages[0].clientWidth;
 
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
 
 //Buttons Listeners
 nextBtn.addEventListener('click', () => {
   if (counter >= carouselImages.length - 1) return;
   carouselSlide.style.transition = 'transform 0.4s ease-in-out';
   counter++;
-  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
 });
 
 prevBtn.addEventListener('click', () => {
   if (counter <= 0) return;
   carouselSlide.style.transition = 'transform 0.4s ease-in-out';
   counter--;
-  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
 });
 
- carouselSlide.addEventListener('transitionend', () => {
-   if (carouselImages[counter].id === 'last-clone') {
-    carouselSlide.style.transition = "none";
+carouselSlide.addEventListener('transitionend', () => {
+  if (carouselImages[counter].id === 'last-clone') {
+    carouselSlide.style.transition = 'none';
     counter = carouselImages.length - 2;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-   }
-    if (carouselImages[counter].id === 'first-clone') {
-    carouselSlide.style.transition = "none";
+    carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
+  }
+  if (carouselImages[counter].id === 'first-clone') {
+    carouselSlide.style.transition = 'none';
     counter = carouselImages.length - counter;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-   }
+    carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
+  }
+});
 
- });
+// Leafleet map code
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
+L.tileLayer(
+  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token',
+  }
+).addTo(mymap);
